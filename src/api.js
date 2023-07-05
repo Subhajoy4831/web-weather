eb2c4501d076ed0901bf078d985363cf0d48cb2e
Subhajoy Ghosh <ghoshsubhajoy02@gmail.com>
2023-07-05 21:31:45 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "10035e5d96msh452267966d55cedp17a2cfjsn3f067fcc3e8c",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const weatherApiURL = "https://api.openweathermap.org/data/2.5";
-export const weatherApiKey = "1949775c4f51de4a47b157ce0874aff5";
+export const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
